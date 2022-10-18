c64754f4446ffa06570c57104560f3c5fd536572
nabeelgit123 <nabeelskh22@gmail.com>
2022-10-18 19:48:11 +0530

    Locators And Intereaction With WebElements

--- src/interactionWithwebElements/ClickButtonThree.java
+++ src/interactionWithwebElements/ClickButtonThree.java
@@ -1,16 +1,18 @@
-package locators;
+package interactionWithwebElements;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import utils.ConfigReader;
 
-public class FindById {
+public class ClickButtonThree {
+
 	public static void main(String[] args) {
 		ConfigReader configReader = new ConfigReader();
 		WebDriver driver = configReader.launchUrl();
 		driver.findElement(By.linkText("BUTTONS")).click();
-		driver.findElement(By.id("btn_one")).click();
-		// driver.quit();
+		driver.findElement(By.id("btn_three")).click();
+		driver.quit();
 	}
+
 }
